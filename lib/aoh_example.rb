dfd1300e4adc5e5e7d28e24defde3efcfd7b987e
Jina Strickland <jina30@gmail.com>
2020-07-02 15:26:03 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -2,41 +2,44 @@
 
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+
 
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
-  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  infinite_jest = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+  ]
 end
 
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  [
-    DON_G[:name, :occupation],
-    JOELLE_VD[:name, :occupation],
-    PAT_M[:name, :occupation],
-    KATE_G[:name, :occupation],
-    BRUCE_G[:name, :occupation]
+  infinite_jest = [
+    { name: "Don Gately", occupation: "Live-in Staff" },
+    { name: "Joelle van Dyne", occupation: "Radio Personality" },
+    { name: "Pat Monteseian", occupation: "Staff" },
+    { name: "Kate Gompert", occupation: "None" },
+    { name: "Bruce Green", occupation: "Fan of Mildred" }
   ]
-
-    # people = [
-    #   DON_G[],
-    #   JOELLE_VD[],
-    #   PAT_M[],
-    #   KATE_G[],
-    #   BRUCE_G[],
-    #   ]
 end
+
 
 def aoh_lookup(aoh, row, key)
-
+  aoh[row][key]
 end
+
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  aoh[row][key] = new_value
+  aoh
 end
